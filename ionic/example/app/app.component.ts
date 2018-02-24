b42c8cb42bb99d51f6133c9d7d2dc56f53bcfec3
Hevin <liuhw@jpush.cn>
2018-02-24 13:25:24 +0800

    Add ionic example

---
+++ ionic/example/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { JSMS } from '@jiguang-ionic/jsms'
+
+import { HomePage } from '../pages/home/home';
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any = HomePage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, jsms: JSMS) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+
+      jsms.init();
+      jsms.setDebugMode(true);
+    });
+  }
+}
